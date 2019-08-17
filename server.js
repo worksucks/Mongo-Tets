faef7cbdef4458b789648f463abac91b47f58aed
Tomasz Reizer <t_reizer@yahoo.com>
2019-08-17 14:59:10 +0200

    Heroku + Mongo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb+srv://work_sucks:<Poznan266>@cluster0-ok31t.mongodb.net/test?retryWrites=true&w=majority', {
     useMongoClient: true
 });
 
